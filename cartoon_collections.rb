4e53bb23d08d1267143a46505d52f26068503b7d
Thor Summerall <thor.summerall@gmail.com>
2020-11-20 17:41:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do {|call| call.length >4}
+  array.all? do |call|
     if call.length < 4
       true
     else
